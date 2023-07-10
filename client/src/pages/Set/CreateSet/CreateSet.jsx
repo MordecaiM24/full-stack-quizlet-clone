200599bdb77ced7cf218f528d8113d610c3b5221
Mordecai <mordecai.mengesteab@gmail.com>
2023-07-10 18:10:46 -0400

    Changed set model

--- client/src/pages/Set/CreateSet/CreateSet.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { Error } from "common/Error";
-
-export const CreateSet = () => {
-  return <Error />;
-};
--- server/src/models/CardSet.js
+++ server/src/models/CardSet.js
@@ -1,23 +1,29 @@
 import mongoose from "mongoose";
 
-const CardSetSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  cards: [
-    {
-      order: { type: Number, required: true },
-      term: { type: String, required: true },
-      definition: { type: String, required: true },
+const CardSetSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    cards: [
+      {
+        order: { type: Number, required: true },
+        term: { type: String, required: true },
+        definition: { type: String, required: true },
+      },
+    ],
+    public: { type: Boolean, required: true },
+    userOwner: {
+      ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+      },
+      ownerName: { type: String, required: true },
+      ownerProfPic: { type: String, reguired: true },
     },
-  ],
-  public: { type: Boolean, required: true },
-  userOwner: {
-    ownerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
-      required: true,
-    },
-    ownerName: { type: String, required: true },
+    school: { type: String, required: false },
+    subject: { type: String, required: false },
   },
-});
+  { timestamps: true }
+);
 
 export const CardSetModel = mongoose.model("cardSets", CardSetSchema);
